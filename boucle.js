e48a80646db627e227f78fcf2be2abdf1b230cd5
Pharamp71 <noankyogre@gmail.com>
2026-05-06 16:42:20 +0200

    exo

---
+++ boucle.js
@@ -0,0 +1,32 @@
+for (let i = 1; i < 5; i++) {
+    console.log("coucou")
+}
+
+for (let i = 1; i < 101; i++) {
+    console.log(i)
+}
+
+for (let i = 100; i > 0; i--) {
+    console.log(i)
+}
+
+// ecrit une fondation qui affiche la table de multiplication d'un nombre passé en paramètre
+// afficher la table de multiplication d'un ombre quelconque
+// parametre : nombre 
+// return rien
+function affichermultiplication(nbrAmultiplier){
+
+for(let i = 1; i<=10; i++){
+    console.log(nbrAmultiplier*1)
+}
+
+
+}
+
+function tablemultiplication(nombremultiplier){
+
+for (let i = 1; i<=10; i++){
+    console.log(`${nombremultiplier} x ${i} = ${nombremultiplier * i}`)
+}
+
+}
